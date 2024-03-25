fd766c3ed5dccd6f7b34ccfed5b34e08ed8d30a5
Shinjiwon <32361608+NEW-G1@users.noreply.github.com>
2024-03-25 15:43:57 +0900

    Add files via upload

---
+++ Documentation/Introduction/ddg_assistant.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from phi.assistant import Assistant
+from phi.tools.duckduckgo import DuckDuckGo
+
+assistant = Assistant(tools=[DuckDuckGo()], show_tool_calls=True)
+assistant.print_response("Whats happening in France?", markdown=True)
+
